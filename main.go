6118971aa3750e7903a472b8b4938750455bdc47
jaimin <jaimin.parmar@bacancy.com>
2024-02-06 15:29:28 +0530

    added server code

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 
 	"socketexample/models"
 
+	"log"
+	"net/http"
+
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -48,8 +52,23 @@ func CreateRouter() {
 	router = mux.NewRouter()
 }
 
+func InititalizeRoutes() {
+	router.Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header")
+	})
+	router.Handle("/socket.io/", Server)
+}
+
+func StartServer() {
+	fmt.Println("Server Started at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Access-Control-Allow-Origin", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(router)))
+}
+
 func main() {
-	fmt.Println("main function")
 	LoadSocket()
 	CreateRouter()
+	InititalizeRoutes()
+	StartServer()
 }
